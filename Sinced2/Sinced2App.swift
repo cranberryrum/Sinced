2829790dd06afbf9c9569e68dc076c966588fc64
Aditya <adityakoltedes@gmail.com>
2025-10-31 16:53:16 +0530

    Initial Commit

---
+++ Sinced2/Sinced2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sinced2App.swift
+//  Sinced2
+//
+//  Created by Aditya Kolte on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Sinced2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sinced2Tests/Sinced2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sinced2Tests.swift
+//  Sinced2Tests
+//
+//  Created by Aditya Kolte on 31/10/25.
+//
+
+import Testing
+@testable import Sinced2
+
+struct Sinced2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
